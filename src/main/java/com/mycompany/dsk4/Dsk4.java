ba68d8a5aa8306cfc47283d3ea101397c18c475d
fosar <63219760+Oscar-CR09@users.noreply.github.com>
2025-04-14 00:01:32 -0600

    mi primer commit de empleados

---
+++ src/main/java/com/mycompany/dsk4/Dsk4.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.dsk4;
+
+/**
+ *
+ * @author oscar
+ */
+public class Dsk4 {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
